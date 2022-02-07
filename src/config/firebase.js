a9a1d06dd8f4c8a05a326ffee1a260d0ef40e82e
galihif <galihindra2401@mail.ugm.ac.id>
2022-02-07 13:52:24 +0700

    firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCyRLWrNrzcQ07BWdCk8O-95qvjFXr9PRI",
+    authDomain: "gif-tech.firebaseapp.com",
+    projectId: "gif-tech",
+    storageBucket: "gif-tech.appspot.com",
+    messagingSenderId: "26255106893",
+    appId: "1:26255106893:web:176a500ecb0c1637d94d04"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
