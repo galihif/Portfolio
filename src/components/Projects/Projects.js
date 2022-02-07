f01f5f6d326926003f86a8dfddbeb23293b084ca
galihif <galihindra2401@mail.ugm.ac.id>
2022-02-07 17:43:07 +0700

    get project data from firestore

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState, useEffect, useCallback  } from 'react'
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
+
+import { collection, query, where, onSnapshot } from "firebase/firestore";
+import {db} from '../../config/firebase'
 
 import taskmaster from "../../Assets/Projects/taskmaster.png";
 import meraihMimpi from "../../Assets/Projects/meraih-mimpi.png";
@@ -9,7 +12,28 @@ import mbolang from "../../Assets/Projects/mbolang.png";
 import covid from "../../Assets/Projects/covid.png";
 import galgadut from "../../Assets/Projects/galgadut.png";
 
-function Projects() {
+const Projects = () => {
+  const [projects, setProjects] = useState([])
+  const [isLoading, setLoading] = useState(false)
+
+  useEffect(() => {
+    getProjects()
+  },[])
+  
+  const getProjects = ()=>{
+    setLoading(true)
+    const q = query(collection(db, "projects"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach((doc) => {
+        items.push(doc.data());
+        console.log(doc.data())
+      });
+      setProjects(items)
+      setLoading(false)
+    })
+  }
+
   return (
     <Container fluid className="project-section">
       <Particle />
@@ -20,53 +44,28 @@ function Projects() {
         <p style={{ color: "white" }}>
           Here are a few projects I've worked on recently.
         </p>
-        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={taskmaster}
-              isBlog={false}
-              title="Meraih Mimpi Android App"
-              description="This is the Meraih Mimpi Mobile App. This is an android application to record every dream that we will achieve. With this application, it will easier to find out the dreams that we will achieve soon."
-              link="https://s.id/MeraihMimpiRepo"
-            />
-          </Col>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={meraihMimpi}
-              isBlog={false}
-              title="Meraih Mimpi Android App"
-              description="This is the Meraih Mimpi Mobile App. This is an android application to record every dream that we will achieve. With this application, it will easier to find out the dreams that we will achieve soon."
-              link="https://s.id/MeraihMimpiRepo"
-            />
-          </Col>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={mbolang}
-              isBlog={false}
-              title="Mbolang Web Design"
-              description="This is Mbolang Web Design. Mbolang is a travel guide website that provides information and guides about a destination. You can review a destination or write a guide that can help other travelers."
-              link="https://s.id/MbolangProto"
-            />
-          </Col>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={covid}
-              isBlog={false}
-              title="Cov-ID Mobile App"
-              description="This is the Cov-ID Mobile App. This application showing the data of covid 19 cases in Indonesia and in the Indonesian province. I used the public API provided by kawalcovid19.id."
-              link="https://galihif.github.io/covid-app"
-            />
-          </Col>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={galgadut}
-              isBlog={false}
-              title="Galgadut Portfolio Web"
-              description="This is the Galgadut portfolio website. I create this project while I am learning front-end web development in @sandhikagalih 's youtube channel. In his video, he taught me to make a personal portfolio website using CSS framework materialize"
-              link="https://galihif.github.io/galgadut"
-            />
-          </Col>
-        </Row>
+        {
+          isLoading ? null : (
+            <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+              {
+                projects.map((project) => {
+                  return(
+                    <Col md={4} className="project-card">
+                      <ProjectCard
+                        imgPath={project.image}
+                        isBlog={false}
+                        title={project.name}
+                        description={project.desc}
+                        link={project.link}
+                      />
+                      <p>sdasd</p>
+                    </Col>
+                  )
+                })
+              }
+            </Row>
+          )
+        }
       </Container>
     </Container>
   );
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,5 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-import 'firebase/auth'
-import 'firebase/storage'
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -13,8 +11,5 @@ const firebaseConfig = {
     appId: "1:26255106893:web:7169e61b16d4940fd94d04"
 };
 
-firebase.initializeApp(firebaseConfig)
-
-export default firebase
-export const storage = firebase.storage()
-export const firestore = firebase.firestore()
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
