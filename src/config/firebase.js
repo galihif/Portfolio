835ff16fd44754a493a0b0862f65e3b80a923497
galihif <galihindra2401@mail.ugm.ac.id>
2022-02-07 16:37:27 +0700

    add firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,9 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+import 'firebase/storage'
 
-// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyCyRLWrNrzcQ07BWdCk8O-95qvjFXr9PRI",
     authDomain: "gif-tech.firebaseapp.com",
@@ -13,5 +13,8 @@ const firebaseConfig = {
     appId: "1:26255106893:web:7169e61b16d4940fd94d04"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
+
+export default firebase
+export const storage = firebase.storage()
+export const firestore = firebase.firestore()
